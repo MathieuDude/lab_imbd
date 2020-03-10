7deda307a7b9f421d60123523002a6c71711405f
Mathieu Labelle <mlabelle1999@hotmail.com>
2020-03-09 20:53:52 -0400

    Removed import of react-select

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import SearchForm from './components/SearchForm';
 import Header from './components/Header';
 import FilmItem from './components/FilmItem';
 import InputBar from './components/InputBar';
-import Select from 'react-select';
 
 export default function App() {
     // const fetchUrl = 'https://movies-420kcg-lg.herokuapp.com';
